c303153c106ead9d99bad26d95f94588be666b72
GODSWILLK <kainegodswill@gmail.com>
2023-03-21 13:09:18 -0700

    cisfun

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
-#include "main.h"
+include "main.h"
 
 /**
-* print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void)
+int main(void)
 {
 	int i;
 
