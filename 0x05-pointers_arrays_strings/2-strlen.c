4da6f0a390d30ec5434e1faa0531ec7879998dd6
GODSWILLK <kainegodswill@gmail.com>
2023-10-23 01:45:00 -0700

    new files

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+
+ * _strlen - returns d leng of a string
+
+ * @s: string
+
+ * Return: lengt
+*/
+
+int _strlen(char *s)
+
+{
+
+        int longi = 0;
+
+        while (*s != '\0')
+
+        {
+
+                longi++;
+
+                s++;
+
+        }
+
+        return (longi);
+}
